66ba733579814cdec9effaec9e6f16cf03d6c001
Yuting Zhou <zyt1231@gmail.com>
2017-01-08 22:36:46 -0500

    add run

--- src/main/java/LinkedListRemove.java
+++ src/main/java/LinkedListRemove.java
@@ -6,13 +6,13 @@ import java.util.LinkedList;
 
 public class LinkedListRemove {
     public class ListNode {
-        int val;
+        private int val;
         ListNode next;
         ListNode(int x) {
             val = x;
         }
     }
-    
+
     public void removeNode(ListNode node) {
             if(node==null){
                 return;
@@ -21,8 +21,20 @@ public class LinkedListRemove {
         node.next=node.next.next;
     }
 
+    public void run(){
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        one.next = two;
+        two.next = three;
+        System.out.println(one.next.val);
+        removeNode(two);
+        System.out.println(one.next.val);
 
+    }
     public static void main(String[] args) {
+        LinkedListRemove r = new LinkedListRemove();
+        r.run();
         System.out.println();
     }
 }
