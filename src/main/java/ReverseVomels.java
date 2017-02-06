579859201c918299d3439ca0369e7c6c0c8861c0
Yuting <zyt1231@gmail.com>
2017-02-06 14:33:52 -0500

    Create ReverseVomels.java

---
+++ src/main/java/ReverseVomels.java
@@ -0,0 +1,42 @@
+class ReverseVomels
+{
+    public static boolean ifVomel(char c){
+        String vowels = "aeiouAEIOU";
+        if (vowels.indexOf(c)>=0){
+            return true;
+        }
+        return false;
+    }
+    public static String reverseVomels(String s){
+        char[] chars = s.toCharArray();
+        int pStart = 0;
+        int pEnd = s.length()-1;
+        char tmp;
+        while(pStart<pEnd){
+            char c = s.charAt(pStart);
+            if(ifVomel(chars[pStart]) && ifVomel(chars[pEnd])){
+                tmp = chars[pEnd];
+                chars[pEnd] = chars[pStart];
+                chars[pStart] = tmp;
+                pStart++;
+                pEnd--;
+            }
+            else{
+                if(!ifVomel(chars[pStart])){
+                    pStart ++;
+                }
+                if(!ifVomel(chars[pEnd])){
+                    pEnd--;
+                }
+            }
+        }
+        
+        return new String(chars); 
+    }
+    
+     public static void main(String []args){
+         System.out.println(ifVomel('d'));
+         System.out.println(reverseVomels("leetcode"));
+         System.out.println(reverseVomels("hello"));
+     }
+}
