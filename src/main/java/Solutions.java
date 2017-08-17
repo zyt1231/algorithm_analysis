ec97e3e6d3564f57bc57dabae0a4add4d9a75e41
Yuting <zyt1231@gmail.com>
2017-08-17 07:43:04 -0400

    binary search

--- src/main/java/Solutions.java
+++ src/main/java/Solutions.java
@@ -4,6 +4,21 @@ import java.util.*;
  */
 public class Solutions {
 //-------------------------------------------------------------------------------------------------------------------------------
+    public int searchInsert_35(int[] nums, int target) {
+      if(nums.length==0) return 0;
+            int low=0;
+            int high=nums.length-1;
+            while(low<=high){
+                int mid = low + (high-low)/2;
+                if(target==nums[mid]){
+                    return mid;
+                }
+                if(target>nums[mid]) low=mid+1;
+                if(target<nums[mid]) high=mid-1;
+            }
+            return low;
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
     public int strStr_28(String haystack, String needle) {
         //needle and haystack, substring and string
         for(int i=0; ; i++){
