9795785b949465d1bb1f19cdd63009ca39c38594
Yuting <zyt1231@gmail.com>
2017-08-21 08:13:58 -0400

    Update Solutions.java

--- src/main/java/Solutions.java
+++ src/main/java/Solutions.java
@@ -4,6 +4,20 @@ import java.util.*;
  */
 public class Solutions {
 //-------------------------------------------------------------------------------------------------------------------------------
+    public int climbStairs_70(int n) {
+        //dp
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
     public int lengthOfLastWord_58(String s) {
         return s.trim().length() - s.trim().lastIndexOf(" ") -1;
     }
