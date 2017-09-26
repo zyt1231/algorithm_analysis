7cd697862225640d3cedfbb1ebfce9d3d25a0316
Yuting <zyt1231@gmail.com>
2017-09-26 09:47:06 -0400

    min stack class

--- src/main/java/Solutions.java
+++ src/main/java/Solutions.java
@@ -1,4 +1,38 @@
 import java.util.*;
+public class Solution{
+//-------------------------------------------------------------------------------------------------------------------------------
+// class MinStack {
+//     /** initialize your data structure here. */
+//     int min=Integer.MAX_VALUE;
+//     Stack<Integer> s = new Stack<Integer>();
+//     public MinStack() {
+//     }
+//     public void push(int x) {
+//         if(x<=min){
+//             s.push(min);
+//             min=x;
+//         }
+//         s.push(x);
+//     }
+//     public void pop() {
+//         if(s.pop()==min) min=s.pop();
+//     }
+//     public int top() {
+//         return s.peek();
+//     }
+//     public int getMin() {
+//         return min;
+//     }
+// }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 //-------------------------------------------------------------------------------------------------------------------------------
     public int singleNumber_136(int[] nums) {
       if(nums.length==0) throw new IllegalArgumentException();
