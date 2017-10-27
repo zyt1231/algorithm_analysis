2939ae20bb7e41347698b13bf625794b817fa87d
Yuting <zyt1231@gmail.com>
2017-10-27 11:47:37 -0400

    max production of word length

--- src/main/java/Solutions.java
+++ src/main/java/Solutions.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class Solution{
 //-------------------------------------------------------------------------------------------------------------------------------
+    public int maxProduct_318(String[] words) {
+        if(words.length==0 || words==null) return 0;
+        int[] values = new int[words.length];
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words[i].length();j++){
+                values[i]|=1<<(words[i].charAt(j)-'a');
+            }
+        }
+        int maxProduct = 0;
+        for(int i=0;i<values.length;i++){
+            for(int j=i+1;j<values.length;j++){
+                if((values[i] & values[j])==0 && (words[i].length()*words[j].length())>maxProduct){
+                    maxProduct = words[i].length()*words[j].length();
+                }
+            }
+        }
+        return maxProduct;
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
     public int countPrimes_204(int n) {
         boolean[] notPrime = new boolean[n]; //use notPrime instead of prime because default false for each element in the array
         int count = 0;
